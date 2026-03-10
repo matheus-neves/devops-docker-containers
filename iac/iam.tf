f80c1cb1e9a7d4d6377b5840d09194a53f0b9201
Matheus <matheusneves1989@gmail.com>
2026-03-10 17:29:15 -0300

    setup iac

---
+++ iac/iam.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_openid_connect_provider" "open_id_connect_git" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+
+  tags = {
+    IAC = "true"
+  }
+}
+
+resource "aws_iam_role" "ecr-role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    "Version" = "2012-10-17"
+    "Statement" = [
+      {
+        "Effect" = "Allow"
+        "Action" = "sts:AssumeRoleWithWebIdentity"
+        "Principal" = {
+          "Federated" = "arn:aws:iam::296348348274:oidc-provider/token.actions.githubusercontent.com"
+        }
+        "Condition" = {
+          "StringEquals" = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          "StringLike" = {
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:matheus-neves/devops-docker-containers:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    IAC = "true"
+  }
+}
